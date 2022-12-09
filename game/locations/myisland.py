b7dddcb879f7d0c92a00d84b537f1fed1b687bb7
Thomas Fodera <tfode001@plattsburgh.edu>
2022-12-09 17:50:24 -0500

    added sublocations
    added the mountain top to fight the sword master, the tavern to get a drink, and the house of a cartographer to get your reward. Do these areas actually do anything yet? No. Are they functional? Arguably yes.

--- game/locations/myisland.py
+++ game/locations/myisland.py
@@ -21,6 +21,9 @@ class Island (location.Location):
         self.locations = {}
         self.locations["port"] = self.starting_location
         self.locations["field"] = Field(self)
+        self.locations["mountain"] = Mountaintop(self)
+        self.locations["tavern"] = Tavern(self)
+        self.locations["house"] = House(self)
 
     def enter (self, ship):
         print ("You see an island approaching. Rather, You are approaching an island, but still.")
@@ -70,17 +73,79 @@ class Field (location.SubLocation):
 
         self.verbs['fight'] = self
     def enter (self):
-        description = "You walk into an open field, full of pirates looking for a fight."
+        description = 'You walk into an open field, full of pirates looking for a fight. To the north, you see a large mountain. To the east, you see a winding path. To the west, you see a building with a sign that reads "Scumm Bar".'
         announce (description)
     def process_verb (self, verb, cmd_list, nouns):
-        if (verb == "south" or verb == "north" or verb == "east" or verb == "west"):
+        if verb == "south":
             config.the_player.next_loc = self.main_location.locations["port"]
             #eventually this is gonna just be for south, i'll have something else north east and west
+        if verb == "north":
+            config.the_player.next_loc = self.main_location.locations["mountain"]
+        if verb == "east":
+            config.the_player.next_loc = self.main_location.locations["house"]
+        if verb == "west":
+            config.the_player.next_loc = self.main_location.locations["tavern"]
         if verb == "fight":
             battle = Battle(Pirate())
             battle.fight()
-            #announce("You wonder when people will be added to this island. Somehow, somewhere, you hear a programmer sigh.")
+            announce(description)
 
+class Mountaintop (location.SubLocation):
+    def __init__ (self, m):
+        super().__init__(m)
+        self.name = "mountain"
+        self.verbs['south'] = self
+        
+        self.verbs['challenge'] = self
+    
+    def enter(self):
+        description = 'You make your way up the mountain and find yourself at a large, flat arena at its peak. The path back to the base snakes down to the south. A skilled swordswoman stands at its center, awaiting a worthy challenge.'
+        announce(description)
+        
+    def process_verb (self, verb, cmd_list, nouns):
+        if verb == "south":
+            config.the_player.next_loc = self.main_location.locations["field"]
+        if verb == "challenge":
+            if len(Battle.known_openings) != 6:
+                announce("The Sword Master laughs at your challenge. It seems that you're not even worth her time at your skill level.")
+
+class Tavern (location.SubLocation):
+    def __init__ (self, m):
+        super().__init__(m)
+        self.name = "tavern"
+        self.verbs['east'] = self
+        
+        self.verbs['talk'] = self
+        self.verbs['drink'] = self
+        
+    def enter(self):
+        description = "You enter the Scumm Bar. Unfortunately, the programmer doesn't remember what the Scumm Bar looks like at the moment, so there's nobody to talk to, and nothing to drink. Feel free to show yourself to the door and back east"
+        announce(description)
+    
+    def process_verb (self, verb, cmd_list, nouns):
+        if verb == "east":
+            config.the_player.next_loc = self.main_location.locations["field"]
+        if (verb == "talk" or verb == "drink"):
+            announce("work in progress")
+
+class House (location.SubLocation):
+    def __init__ (self, m):
+        super().__init__(m)
+        self.name = "house"
+        self.verbs['west'] = self
+        
+        self.verbs['talk'] = self
+    def enter(self):
+        description = "You follow the path to the house of a local cartographer, who sits behind a desk studying his maps. Perhaps he has an idea where your home port is. Otherwise, the path back leads you to the west."
+        announce(description)
+    
+    def process_verb (self, verb, cmd_list, nouns):
+        if verb == "west":
+            config.the_player.next_loc = self.main_location.locations["field"]
+        if verb == "talk":
+            announce("You'd love to talk to the cartographer, but it seems that he's so busy that he forgot to give himself a talk function. Get back to me on that later.")
+
+        
 #All Characters and Enemy types
 class Stan(Character):
     def __init__(self):
@@ -99,8 +164,8 @@ class Pirate(Enemy):
             r.append(v)
         super().__init__(n, o, r, random.choice([True, False]))
  
-# class SwordMaster(Enemy):
-    # def __init__(self):
-        # openings = ["This is a test!"]
-        # responses = ["This is a different test!"]
-        # super().__init__("Sword Master", openings, responses, random.choice([True, False])
+class SwordMaster(Enemy):
+    def __init__(self):
+        o = ["MasterOpening1", "MasterOpening2" ]
+        r = ["MasterResponse1", "MasterResponse2"]
+        super().__init__("Sword Master", o, r, True)
